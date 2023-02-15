514cba889eca0598feecd23b7a84dc1ed62fcccd
Lakshay <lakshaydcoder@gmail.com>
2023-02-15 22:17:43 +0530

    log out issue fixed

--- phpFiles/logOut.php
+++ phpFiles/logOut.php
@@ -1,7 +1,8 @@
 <?php
+session_start();
 session_destroy();
 echo "<script>alert('Successfully Logged Out.');</script>";
-        
+
 echo "<script>
         history.go(-2);
         var auth2 = gapi.auth2.getAuthInstance();
