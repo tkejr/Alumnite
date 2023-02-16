fdb7d79f817724a36bd7455de3efe5d3174920f9
Lakshay <lakshaydcoder@gmail.com>
2023-02-16 11:26:33 +0530

    conn

--- phpFiles/connection.php
+++ phpFiles/connection.php
@@ -2,15 +2,15 @@
 
 // mysql: //b9dbb7bf494eea:c56f3c72@us-cdbr-east-06.cleardb.net/heroku_509f154ed7ef340?reconnect=true
 
-// $servername = "us-cdbr-east-06.cleardb.net";
-// $username = "b9dbb7bf494eea";
-// $password = "c56f3c72";
-// $databaseName = "heroku_509f154ed7ef340";
+$servername = "us-cdbr-east-06.cleardb.net";
+$username = "b9dbb7bf494eea";
+$password = "c56f3c72";
+$databaseName = "heroku_509f154ed7ef340";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$databaseName = "alumnite";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $databaseName = "alumnite";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $databaseName);
